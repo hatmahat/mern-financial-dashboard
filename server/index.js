2820564c74ca0019906c66f9d3765a2f4d8c5d0b
Mahatma <hatmahat@gmail.com>
2023-05-18 17:59:19 +0700

    routing and modeling

--- server/index.js
+++ server/index.js
@@ -4,17 +4,36 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
-import morgan  from "morgan";
+import morgan from "morgan";
+import kpiRoutes from "./routes/kpi.js";
+import KPI from "./models/KPI.js";
+import { kpis } from "./data/data.js";
 
 /* CONFIGURATIONS */
-dotenv.config()
+dotenv.config();
 const app = express();
-app.use(express.json())
-app.use(helmet())
-app.use(helmet.crossOriginResourcePolicy({policy: "cross-origin"}))
-app.use(morgan("common"))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extend: false}))
-app.use(cors())
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extend: false }));
+app.use(cors());
+
+app.use("/kpi", kpiRoutes);
 
 /* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(async () => {
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+        /* ADD DATA ONE TIME ONLY OR AS NEEDED */
+        // await mongoose.connection.db.dropDatabase(); // dont use this in production
+        // KPI.insertMany(kpis);
+    })
+    .catch((error) => console.log(`${error} did not connect`));
